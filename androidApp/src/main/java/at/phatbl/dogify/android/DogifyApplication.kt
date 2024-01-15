df8f67bbe238994ba7159a92115211ef9ec55359
Ben Chatelain <ben@octop.ad>
2024-01-15 09:29:39 -0700

    ✨ DogifyApplication

---
+++ androidApp/src/main/java/at/phatbl/dogify/android/DogifyApplication.kt
@@ -0,0 +1,11 @@
+package at.phatbl.dogify.android
+
+import android.app.Application
+import at.phatbl.dogify.di.initKoin
+
+class DogifyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+}
--- androidApp/src/main/java/at/phatbl/dogify/android/MainActivity.kt
+++ androidApp/src/main/java/at/phatbl/dogify/android/MainActivity.kt
@@ -6,8 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import at.phatbl.dogify.Breed
+import at.phatbl.dogify.FetchBreedsUseCase
+import at.phatbl.dogify.GetBreedsUseCase
+import at.phatbl.dogify.ToggleFavouriteStateUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+suspend fun greet() = "${FetchBreedsUseCase().invoke()}\n" +
+    "${GetBreedsUseCase().invoke()}\n" +
+    "${ToggleFavouriteStateUseCase().invoke(
+        Breed ("toggle favourite state test", "")
+    )}\n"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView("Hello World")
+                    GreetingView()
                 }
             }
         }
@@ -26,14 +43,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView() {
+    val myState = remember { mutableStateOf("initial state") }
+
+    LaunchedEffect(Unit) {
+        // Call the suspend function using launch
+        val result = withContext(Dispatchers.IO) {
+            greet()
+        }
+        myState.value = result
+    }
+
+    Text(text = myState.value)
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView()
     }
 }
