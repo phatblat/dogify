edac1186c4ea344d449023f00c2607960befa143
Ben Chatelain <ben@octop.ad>
2024-03-24 17:59:46 -0600

    ➕ sqdelight gradle plugin (2.0.1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.sqldelight).apply(false)
 }
 
 // Value stored in gradle.properties
